819c0d7d34ddbcae929a610d05803e7c1093c2b3
Isaac Donado <idonado@f1r3s8.codam.nl>
2022-02-06 22:00:16 +0100

    Need a linux/posix aproach for looping through fd_set

--- tcp-multiple-clients/main.cpp
+++ tcp-multiple-clients/main.cpp
@@ -7,6 +7,7 @@
 #include <arpa/inet.h> // for htons()
 #include <netdb.h>  // Definitions for default buffer sizes.
 #include <sys/select.h> // For select()
+#include <sys/time.h> // for select
 
 int         server_port;
 std::string server_password;
@@ -59,15 +60,65 @@ int main(int argc, char** argv)
     }
 
     // Select magic
-    fd_set  master;
+    fd_set  master; 
     fd_set  copy;
     int     socket_count;
     FD_ZERO(&master);           // Initialized "master" set and cleared it for safety. 
     FD_SET(socket_fd, &master); // Add main socket to the set. (?)
+    int     max_fd = socket_fd;
+    int     current_socket;
+    int     new_client;
+
+    // Client info struct & more
+    sockaddr_in client_info;
+    socklen_t  client_infoSize = sizeof(client_info);
+    char        buff[4096];
+    int         bytes_received;
+
     while(true)
     {
-        // Copy since select destroys "unneeded" info from struct
-        copy = master; 
+        // Copy since select destroys "uneeded" info from struct
+        copy = master;
+        socket_count = select(max_fd, &copy, NULL, NULL, NULL);
+        for (int i = 0 ; i < socket_count ; i++)
+        {
+            current_socket = copy.fd_array[i];      // fd_array doesn't exist in fd_set in linux/macos ... Need another approach
+            if (current_socket == socket_fd)
+            {
+                // New connection.
+
+
+                // accept() new connection
+                new_client = accept(socket_fd, (sockaddr*)&client_info, &client_infoSize);
+                if (new_client < 0)
+                {
+                    close(socket_fd);
+                    error_exit("[107]Error on acceptiong new connection", 107);
+                }
+                std::cout << "Accepted connection from: " << inet_ntoa(client_info.sin_addr) << std::endl;
+                FD_SET(new_client, &master);    // Adding to master list of FDs
+                if (new_client > max_fd)
+                    max_fd = new_client;
+            }
+            else
+            {
+                // Message from connected client.
+
+                // recv() actual message
+                memset((void*)buff, 0, 4096);
+                bytes_received = recv(current_socket, &buff, 4096, 0);
+                if (bytes_received <= 0)
+                {
+                   std::cout << "Error or connection lost with " << inet_ntoa(client_info.sin_addr);
+                   close(current_socket);
+                   FD_CLR(current_socket, &master);
+                }
+                else
+                {
+                    std::cout << "[RECEIVED]: " << std::string(buff, 0, bytes_received);
+                }
+            }
+        }
     }
 
 
